74574d8ae3a3ee26f02f46f806c38025d1eed84d
MakerTim <MakerTim@outlook.com>
2018-03-30 22:19:31 +0200

    Registering of items! v2

--- src/main/java/net/modjam5/makercommunity/common/Registry.java
+++ src/main/java/net/modjam5/makercommunity/common/Registry.java
@@ -1,11 +1,12 @@
 package net.modjam5.makercommunity.common;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * @author Tim Biesenbeek
@@ -17,7 +18,13 @@ public class Registry {
 	}
 
 	public void init(FMLInitializationEvent event) {
+
+	}
+
+	@SubscribeEvent
+	public void registerBlocks(RegistryEvent.Register<Item> event) {
 		ItemRegistry.register();
+		event.getRegistry().registerAll(ItemRegistry.items);
 	}
 
 	public void startWorld(FMLServerStartingEvent event) {
