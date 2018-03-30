96f28b5041a03fd3a966104442f04ed1d06ab995
MakerTim <MakerTim@outlook.com>
2018-03-30 23:03:24 +0200

    Item Texture registered :D

--- src/main/java/net/modjam5/makercommunity/client/ClientRegistry.java
+++ src/main/java/net/modjam5/makercommunity/client/ClientRegistry.java
@@ -1,6 +1,12 @@
 package net.modjam5.makercommunity.client;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.modjam5.makercommunity.common.ItemRegistry;
 import net.modjam5.makercommunity.common.Registry;
 import net.modjam5.makercommunity.util.SoundUtil;
 import net.modjam5.makercommunity.worldmusic.MusicWorldHelper;
@@ -16,6 +22,15 @@ public class ClientRegistry extends Registry {
 	public void init(FMLInitializationEvent event) {
 		super.init(event);
 		registerSounds();
+
+	}
+
+	@SubscribeEvent
+	public void onModelRegister(ModelRegistryEvent event) {
+		for (Item item : ItemRegistry.items) {
+			ModelLoader.setCustomModelResourceLocation(item, 0,
+				new ModelResourceLocation(item.getRegistryName(), null));
+		}
 	}
 
 	private void registerSounds() {
